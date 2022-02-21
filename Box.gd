907fd24a848722354813f7f912e45fb013fe1971
Wowe-Peanut <47678647+Wowe-Peanut@users.noreply.github.com>
2022-02-21 12:26:57 -0600

    Box now raycasts each vertex

--- Box.gd
+++ Box.gd
@@ -1,5 +1,6 @@
 extends RigidBody
 
+onready var mesh = $MeshInstance
 var held: bool = false
 
 # Called when the node enters the scene tree for the first time.
@@ -9,10 +10,17 @@ func _ready():
 
 func _physics_process(delta):
 	if !held:
+		#Raycasts from all vertices 
 		var space_state = get_world().direct_space_state
-		var result = space_state.intersect_ray(global_transform.origin, get_viewport().get_camera().global_transform.origin, [self])
-		if result.size() > 0 and result.collider.name != "Player":
-			queue_free()
+		for vertex in mesh.get_mesh().get_faces():
+			var result = space_state.intersect_ray(mesh.global_transform.xform(vertex), get_viewport().get_camera().global_transform.origin, [self])
+			if result.size() <= 0 or result.collider.name == "Player":
+				return
+		queue_free()	
+		
+		
+	
+		
 	
 
 func _on_screen_exited():
