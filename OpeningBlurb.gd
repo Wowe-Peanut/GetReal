6aefd399f811cf8f2925c01847a4406a5b6f73b3
Pillber <melcard16@gmail.com>
2022-02-22 21:03:19 -0600

    theming!

---
+++ OpeningBlurb.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+onready var animation_player = $AnimationPlayer
+onready var timer = $Timer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	$BlurbCenter/Blurb.percent_visible = 0.0
+	$TaglineCenter/Tagline.percent_visible = 0.0
+	
+	timer.start()
+	yield(timer, "timeout")
+	
+	animation_player.play("Blurb")
+	yield(animation_player, "animation_finished")
+	
+	timer.start()
+	yield(timer, "timeout")
+	$BlurbCenter/Blurb.percent_visible = 0.0
+	
+	animation_player.play("Tagline")
+	yield(animation_player, "animation_finished")
+	
+	timer.start()
+	yield(timer, "timeout")
+	
+	get_tree().change_scene("res://Levels/Level1.tscn")
+	
