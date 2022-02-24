d502b7e166940306771c59f24e4926c9a19f7643
Wowe-Peanut <47678647+Wowe-Peanut@users.noreply.github.com>
2022-02-24 14:23:55 -0600

    2 boxs 1 button :trollface:

--- Objects/Button.gd
+++ Objects/Button.gd
@@ -6,6 +6,7 @@ signal triggered()
 export(Array, int) var powers = []
 export(Array, int) var unpowers = []
 var triggered: bool = false
+var trigger_count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,10 +16,14 @@ func _ready():
 
 func _on_area_entered(body):
 	if "Box" in body.name:
+		trigger_count += 1
 		triggered = true
-		emit_signal("triggered")
+		if trigger_count == 1:
+			emit_signal("triggered")
 
 func _on_area_exited(body):
 	if "Box" in body.name:
+		trigger_count -= 1
 		triggered = false
-		emit_signal("triggered")
+		if trigger_count == 0:
+			emit_signal("triggered")
