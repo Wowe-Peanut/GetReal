9700ec106fbe539f698ffc3b0aa78ea7c65a0003
Wowe-Peanut <47678647+Wowe-Peanut@users.noreply.github.com>
2022-02-26 20:46:57 -0600

    Minor Level Fixes

--- Levels/Level2.gd
+++ Levels/Level2.gd
@@ -7,9 +7,7 @@ var play_other_hints: bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	hide_hints()
-	$HUD/HintContainer/BoxHint.show()
-	hint_player.play("BoxHint")
+	show_hint("BoxHint")
 	
 	$Level/BoxHint2.connect("body_entered", self, "_on_box_hint_2_triggered")
 	$Level/ButtonHint.connect("body_entered", self, "_on_button_hint_triggered")
@@ -18,28 +16,22 @@ func _ready():
 func _process(delta):
 	if $Boxes.get_child_count() == 0 and !played_hints[2]:
 		play_other_hints = false
-		hide_hints()
-		print("playing")
-		$HUD/HintContainer/ResetHint.show()
-		hint_player.stop()
-		hint_player.play("ResetHint")
+		show_hint("ResetHint")
 		played_hints[2] = true
 	
 	
 func _on_box_hint_2_triggered(_body):
 	if !played_hints[1] and play_other_hints:
-		hide_hints()
-		$HUD/HintContainer/BoxHint2.show()
-		hint_player.play("BoxHint2")
+		show_hint("BoxHint2")
 		played_hints[1] = true
 		
 func _on_button_hint_triggered(_body):
 	if !played_hints[3] and play_other_hints:
-		hide_hints()
-		$HUD/HintContainer/ButtonHint.show()
-		hint_player.play("ButtonHint")
+		show_hint("ButtonHint")
 		played_hints[3] = true
 	
-func hide_hints():
+func show_hint(show):
 	for hint in $HUD/HintContainer.get_children():
-		hint.hide()
+		if hint.name != show:
+			hint.modulate = Color(0,0,0,0)
+		hint_player.play(show)
