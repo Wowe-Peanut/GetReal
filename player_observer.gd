d4a14df174e31428e4dfbdf8e8bab3a1168cf392
Pillber <melcard16@gmail.com>
2023-05-09 23:51:48 -0500

    fix player observer overriding LOS through other observers

--- player_observer.gd
+++ player_observer.gd
@@ -6,15 +6,20 @@ func _physics_process(_delta) -> void:
 	var boxes = get_tree().get_nodes_in_group("box")
 	
 	var state = get_world_3d().direct_space_state
-	print(seen)
+	
+	seen.sort_custom(func(a, b): return a.is_in_group("box") and not b.is_in_group("box"))
+	print("seen: ", seen)
+	
 	for observed_object in seen:
 		if observed_object.is_in_group("box"):
 			var obscured = is_obscured(state, observed_object)
 			modify_observed(observed_object, obscured, true)
 		else:
-			check_observer(state, observed_object.observer)
+			is_observer_obscured(state, observed_object.observer)
+			
+	#when observer checks before the player, the player unable to see the box overrides the observer check
 	
-	print(observed)
+	print("observed: ", observed)
 	# iterate in all the boxes the player can see (even through other observers)
 	for box in observed:
 		# stop coyote time if started
@@ -28,14 +33,6 @@ func _physics_process(_delta) -> void:
 		if box.coyote_timer.is_stopped(): box.coyote_timer.start()
 		pass
 
-func modify_observed(observed_object: RigidBody3D, obscured: bool, direct_player) -> void:
-	if obscured:
-		#print("direct player ", direct_player)
-		observed.erase(observed_object)
-	elif not observed.has(observed_object):
-		observed.append(observed_object)
-
-
 func is_obscured(state: PhysicsDirectSpaceState3D, box: RigidBody3D) -> bool:
 	for vertex in remove_duplicate(box.mesh.mesh.get_faces()):
 		# cast a ray from the box to the player cam
@@ -44,13 +41,12 @@ func is_obscured(state: PhysicsDirectSpaceState3D, box: RigidBody3D) -> bool:
 		if result.collider == self_collider:
 			return false
 	return true
-
-func check_observer(state: PhysicsDirectSpaceState3D, observer: Observer) -> void:
+		
+func is_observer_obscured(state: PhysicsDirectSpaceState3D, observer: Observer) -> void:
 	for observed_object in observer.seen:
 		if observed_object.is_in_group("box"):
 			var obscured = is_obscured_through_observer(state, observer, observed_object)
 			modify_observed(observed_object, obscured, false)
-
 
 func is_obscured_through_observer(state: PhysicsDirectSpaceState3D, observer: Observer, box: RigidBody3D) -> bool:
 	for vertex in remove_duplicate(box.mesh.mesh.get_faces()):
@@ -63,6 +59,13 @@ func is_obscured_through_observer(state: PhysicsDirectSpaceState3D, observer: Ob
 			if result.collider == self_collider:
 				return false
 	return true
+
+func modify_observed(observed_object: RigidBody3D, obscured: bool, direct_player) -> void:
+	if obscured:
+		#print("direct player ", direct_player)
+		observed.erase(observed_object)
+	elif not observed.has(observed_object):
+		observed.append(observed_object)
 
 func _on_body_exited(body) -> void:
 	if body in observed:
